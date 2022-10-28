ebf1d0225805c1eb4901d9445517b945394d6e39
hareshbaskaran <baskaranharesh@gmail.com>
2022-10-28 20:58:02 +0530

    division method - error 302 not fixed

--- flask_rest.py
+++ flask_rest.py
@@ -2,13 +2,17 @@ from flask import Flask,jsonify,request
 from flask_restful import Api,Resource
 app = Flask(__name__)
 api=Api(app)
-
 def checkPostedData(postedData,functionName):
     if(functionName=="add" or "sub" or "mul"):
         if "x" not in postedData or "y" not in postedData:
             return 301
         else:
             return 200
+    if(functionName=="div"):
+        if "y"==0:
+            return 302
+        else:
+            return 200         
 class Add(Resource):
     def post(self):
     #get posted data:
@@ -76,13 +80,39 @@ class Mul(Resource):
         }
         return jsonify(retMap)
 class Div(Resource):
-    pass
+     def post(self):
+    #get posted data:
+        postedData=request.get_json()
+        status_code=checkPostedData(postedData,"div")
+        if(status_code!=200):
+            retJson={
+                  'message':"error message",
+            'status code':status_code
+            }    
+            return jsonify(retJson)
+        if(status_code==302):
+            retJson={
+                  'message':"Invalid Divisor",
+            'status code':status_code
+            }    
+            return jsonify(retJson)    
+        x=postedData["x"]
+        y=postedData["y"]
+        x=int(x)
+        y=int(y)
+    #Add the posted Data       
+        ret=x/y
+        retMap={
+            'message':ret,
+            'status code':200 
+        }
+        return jsonify(retMap)
 
 #add api resources for created classes
 api.add_resource(Add,"/add")
 api.add_resource(Sub,"/sub")
 api.add_resource(Mul,"/mul")
-
+api.add_resource(Div,"/div")
 @app.route('/')    
 def test():
     return'test'
